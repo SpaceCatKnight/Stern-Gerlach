5171213a19a026d47314299cd5a4400a7ee4f69c
StarNutCracker <rubenbeynon@hotmail.com>
2017-02-20 17:35:03 +0100

    Update Stern-Gerlach.py

--- Stern-Gerlach.py
+++ Stern-Gerlach.py
@@ -17,9 +17,12 @@ a0 = 1.5459*10**(-2)
 a1 = 0.6113
 a2= 0.5146
 a3 = -0.3907
-mI = 5 # error on I in mA IRRELEVANT!!!!
+ma0 = 2.7819*10**(-3)
+ma1 = 2.0927*10**(-2)
+ma2 = 4.1541*10**(-2)
+ma3 = 2.2721*10**(-2)
+mI = 0.005 # error on I in mA IRRELEVANT!!!!
 ml = 1*10**(-3) # error on l in mm 
-mB = 0.01 # geschatzt aus Fehler auf I
 mL = 0.1*10**(-3) # error on L in mm
 ma = 0.2*10**(-3) # error on a in mm
 mep = 0.0026
@@ -30,9 +33,9 @@ mT = 0.1
 I = [0.402,0.600,0.700,0.800,0.900,1.000] # current in A
 #B = [0.320,0.485,0.560,0.635,0.695,0.750] # aus Plot mithilfe von I abgelesen
 TC = [184.1,186.3,186.5,186.5,186.7,187.5] # temperature in C
-T = [457.25, 459.45, 459.65, 459.65, 459.85, 460.65] # temperature in K
+#T = [457.25, 459.45, 459.65, 459.65, 459.85, 460.65] # temperature in K
 q = [0.0022452, 0.0035437, 0.0042096, 0.004781, 0.0053034, 0.005731099999999999] 
-
+T = 459.4
 
 def Bfeld(I,a0,a1,a2,a3):
     B = []
@@ -42,7 +45,20 @@ def Bfeld(I,a0,a1,a2,a3):
     
 B = Bfeld(I,a,a1,a2,a3)
 
+def FehlerBfeld(I,a1,a2,a3):
+    mB = []
+    for i in range(6):
+        mI1 = (mI*(a1+2*a2*I[i]+3*a3*(I[i]**2)))
+        ma01= ma0
+        ma11= ma1*I[i]
+        ma21= ma2*(I[i]**2)
+        ma31= ma3*(I[i]**3)
+        mB.append((mI1**2+ma01**2+ma11**2+ma21**2+ma31**2)**(0.5))
+    return mB
 
+
+mB = FehlerBfeld(I,a1,a2,a3)
+    
 def magnetfeld(ep,a,B):
     dB = []
     for i in range(6):
@@ -53,26 +69,26 @@ def magnetfeld(ep,a,B):
 def Msz(l,L,kb,T,q,dB,ep,a,B):
     M = []
     for i in range(6):
-        M.append((q[i]*2*kb*(T[i]))/(l*L*(1-L/(2*l))*magnetfeld(ep,a,B)[i]))
+        M.append((q[i]*2*kb*(T))/(l*L*(1-L/(2*l))*magnetfeld(ep,a,B)[i]))
     return M
     
 
 
    
 def dq(kb,T,a,l,L,ep,B,i):
-    return 2*kb*T[i]*a/(l*L*(1-L/(2*l))*ep*B[i])
+    return 2*kb*T*a/(l*L*(1-L/(2*l))*ep*B[i])
 def da(kb,T,q,l,L,ep,B,i):
-    return 2*kb*T[i]*q[i]/(l*L*(1-L/(2*l))*ep*B[i])
+    return 2*kb*T*q[i]/(l*L*(1-L/(2*l))*ep*B[i])
 def dT(kb,a,q,l,L,ep,B,i):
     return 2*kb*a*q[i]/(l*L*(1-L/(2*l))*ep*B[i])
 def dl(kb,T,a,q,l,L,ep,B,i):
-    return -8*kb*T[i]*a*q[i]/(L*((L-2*l)**2)*ep*B[i])
+    return -8*kb*T*a*q[i]/(L*((L-2*l)**2)*ep*B[i])
 def dL(kb,T,a,q,l,L,ep,B,i):
-    return -8*kb*T[i]*a*q[i]*(l-L)/((L**2)*((L-2*l)**2)*ep*B[i])   
+    return -8*kb*T*a*q[i]*(l-L)/((L**2)*((L-2*l)**2)*ep*B[i])   
 def dep(kb,T,a,q,l,L,ep,B,i):
-    return -2*kb*T[i]*a*q[i]/(l*L*(1-L/(2*l))*(ep**2)*B[i])   
+    return -2*kb*T*a*q[i]/(l*L*(1-L/(2*l))*(ep**2)*B[i])   
 def dB(kb,T,a,q,l,L,ep,B,i):
-    return -2*kb*T[i]*a*q[i]/(l*L*(1-L/(2*l))*ep*(B[i]**2))
+    return -2*kb*T*a*q[i]/(l*L*(1-L/(2*l))*ep*(B[i]**2))
 
 def Fehler(kb,T,a,q,l,L,ep,B,mq,ma,mT,ml,mL,mep,mB):
     m = []
@@ -85,14 +101,14 @@ def Fehler(kb,T,a,q,l,L,ep,B,mq,ma,mT,ml,mL,mep,mB):
         dL1 = dL(kb,T,a,q,l,L,ep,B,i)
         dep1 = dep(kb,T,a,q,l,L,ep,B,i)
         dB1 = dB(kb,T,a,q,l,L,ep,B,i)
-        m.append(((dq1*mq[i])**2+(da1*ma)**2+(dT1*mT)**2+(dL1*mL)**2+(dl1*ml)**2+(dep1*mep)**2+(dB1*mB)**2)**(0.5))
+        m.append(((dq1*mq[i])**2+(da1*ma)**2+(dT1*mT)**2+(dL1*mL)**2+(dl1*ml)**2+(dep1*mep)**2+(dB1*mB[i])**2)**(0.5))
     return m
     
 ML= Msz(l,L,kb,T,q,dB,ep,a,B)
 FL = Fehler(kb,T,a,q,l,L,ep,B,mq,ma,mT,ml,mL,mep,mB)
 
-
-
+print("mB-Feld",mB)
+print("B-Feld",B)
 print(""     ) 
 print("Werte von m:",ML)
 print("")
@@ -100,5 +116,10 @@ print("Fehler auf m:",FL)
 print("")
 print("Mittelwert m:",np.mean(ML))
 print("Mittelwert Fehler:",np.mean(FL))
-   
 
+"""
+x = np.array([0.0022452, 0.0035437, 0.0042096, 0.004781, 0.0053034, 0.005731099999999999])
+y = np.array([0.3060222679144, 0.4701447999999999, 0.5485538999999999, 0.6208455999999999, 0.6846756999999999, 0.7376999999999998])
+z = np.polyfit(x, y, 1)
+print(z)
+"""
