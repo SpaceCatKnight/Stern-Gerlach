cb8b3b175311a5da92607ea4cda5ce336300cb1c
StarNutCracker <rubenbeynon@hotmail.com>
2017-02-20 15:49:24 +0100

    Update Stern-Gerlach.py

--- Stern-Gerlach.py
+++ Stern-Gerlach.py
@@ -23,13 +23,20 @@ mq = [0.0000015,0.0000025,0.0000029,0.0000032,0.0000032,0.0000041]
 mT = 0.1
 
 
-I = [402,600,700,800,900,1000] # current in mA
-B = [0.320,0.485,0.560,0.635,0.695,0.750] # aus Plot mithilfe von I abgelesen
+I = [0.402,0.600,0.700,0.800,0.900,1.000] # current in A
+#B = [0.320,0.485,0.560,0.635,0.695,0.750] # aus Plot mithilfe von I abgelesen
 TC = [184.1,186.3,186.5,186.5,186.7,187.5] # temperature in C
 T = [457.25, 459.45, 459.65, 459.65, 459.85, 460.65] # temperature in K
 q = [0.0022452, 0.0035437, 0.0042096, 0.004781, 0.0053034, 0.005731099999999999] 
 
 
+def Bfeld(I,a,a1,a2,a3):
+    B = []
+    for i in range(6):
+        B.append((a+I[i]*a1+a2*(I[i]**2)+a3*(I[i]**3)))
+    return B
+    
+B = Bfeld(I,a,a1,a2,a3)
 
 
 def magnetfeld(ep,a,B):
@@ -45,7 +52,7 @@ def Msz(l,L,kb,T,q,dB,ep,a,B):
         M.append((q[i]*2*kb*(T[i]))/(l*L*(1-L/(2*l))*magnetfeld(ep,a,B)[i]))
     return M
     
-ML= Msz(l,L,kb,T,q,dB,ep,a,B)
+
 
    
 def dq(kb,T,a,l,L,ep,B,i):
@@ -76,19 +83,20 @@ def Fehler(kb,T,a,q,l,L,ep,B,mq,ma,mT,ml,mL,mep,mB):
         dB1 = dB(kb,T,a,q,l,L,ep,B,i)
         m.append(((dq1*mq[i])**2+(da1*ma)**2+(dT1*mT)**2+(dL1*mL)**2+(dl1*ml)**2+(dep1*mep)**2+(dB1*mB)**2)**(0.5))
     return m
+    
+ML= Msz(l,L,kb,T,q,dB,ep,a,B)
 FL = Fehler(kb,T,a,q,l,L,ep,B,mq,ma,mT,ml,mL,mep,mB)
 
 
 
-print ""      
-print "Werte von m:",ML
-print ""
-print "Fehler auf m:",FL
-print ""
-print "Mittelwert m:",np.mean(ML)
-print "Mittelwert Fehler:",np.mean(FL)
+print(""     ) 
+print("Werte von m:",ML)
+print("")
+print("Fehler auf m:",FL)
+print("")
+print("Mittelwert m:",np.mean(ML))
+print("Mittelwert Fehler:",np.mean(FL))
    
-    
-    
+
     
     
